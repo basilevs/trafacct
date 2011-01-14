d3bceefb3e940a75fe3719852b0569342c9321ba
gulevich <gulevich@pebble.pebble.local>
2011-01-14 21:01:00 +0600

    Quick sort fixed again

--- Defaults.scala
+++ Defaults.scala
@@ -89,8 +89,9 @@ object Full {
 		def this(i:AccUnit) {this(i.src.host, i.dst.host, i.protocol)}
 	}
 	type AccResult = (Rule, Long)
-	case class Comparator(a:AccResult) extends Ordered[AccResult] {
-		def compare(that:AccResult) = compareBySecond(a, that)
+	case class Comparator(a:AccResult) extends Ordered[Comparator] {
+		override def equals(that:Any) = compare(that.asInstanceOf[Comparator]) == 0
+		def compare(that:Comparator) = compareBySecond(a, that.a)
 	}
 	val runner = new Configured {
 		def run = {
@@ -98,14 +99,15 @@ object Full {
 			val srcs = new AccSources(Defaults.getSrcs)
 			configure(srcs)
 			s.sum(srcs)
-			val data = s.toArray
-			scala.util.Sorting.quickSort(data)(new Comparator(_))
+			implicit def toOrdered(a:AccResult) = new Comparator(a)
+			val data = s.toArray map toOrdered
+			scala.util.Sorting.quickSort(data)
 			val pp = new PrettyPrinter
 			def printAcc(i:AccResult) {
 				implicit def hostToStr(i:Host) = i.toString
 				println(pp.format(i._1.src, i._1.dst, i._1.protocol, i._2.toString))
 			}
-			data.slice(data.length-limit).foreach(printAcc)
+			data.slice(data.length-limit).foreach((c: Comparator) => printAcc(c.a))
 			0
 		}
 	}
